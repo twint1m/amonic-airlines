dd29d7bdba141ad9c9a81eb22335eab5de302c3e
twint1m <twint1m.contact@gmail.com>
2024-09-13 15:47:03 +0300

    added data with fixtures

--- django/django_project/amonic/admin.py
+++ django/django_project/amonic/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Country, Office, Role, User
+from import_export.admin import ImportExportModelAdmin
+from .resources import UserResource
 
-# Register your models here.
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Office)
+class OfficeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'country', 'phone', 'contact')
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+@admin.register(User)
+class UserAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = UserResource
+    list_display = ('id', 'email', 'role', 'first_name', 'last_name', 'office', 'birthdate', 'active')
--- django/django_project/django_project/settings.py
+++ django/django_project/django_project/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'corsheaders',
-    'amonic'
+    'amonic',
+    'import_export'
 ]
 
 MIDDLEWARE = [
@@ -110,6 +111,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -136,3 +144,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
