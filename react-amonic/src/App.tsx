cc1ab1d161c4d3422aa440ddde781d508ee2d1bb
twint1m <twint1m.contact@gmail.com>
2024-09-16 16:34:56 +0300

    added users pages and registry

--- react-amonic/src/App.tsx
+++ react-amonic/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import './App.css';
-
+import Header from "./components/Header";
+import Register from './components/pages/Register'
+import AddUser from './components/pages/AddUser'
+import UsersInfoTable from './components/pages/UsersInfo'
+import EditUserForm from './components/pages/EditUser'
+import UsersActivity from './components/pages/UserActivity'
+import {Outlet} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      Q
-    </div>
+      <div className="App">
+          <Header/>
+          <Outlet/>
+          {/*<Register/>*/}
+          {/*<UsersInfoTable/>*/}
+          {/*<AddUser/>*/}
+          {/*<EditUserForm/>*/}
+          {/*<UsersActivity/>*/}
+      </div>
   );
 }
 
